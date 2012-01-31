9254ffb1c9f6184ffb7f39518c9e0249f74a76e1
Peter Hmíra <peter.hmira@gmail.com>
2012-01-31 03:45:10 +0100

    Testovacia verzia findu

---
+++ cz/cuni/mff/java/du2011x2/JFind.java
@@ -0,0 +1,194 @@
+package cz.cuni.mff.java.du2011x2;
+
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.regex.*;
+
+//import sun.misc.Regexp;
+
+//import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+//import com.sun.org.apache.xerces.internal.impl.xs.identity.Selector.Matcher;
+
+/** DU 1. 
+  * 
+  * @author Peter Hmira
+  */
+public class JFind {
+	static class MyFinder
+	{
+		private static String[] Conditions;
+		
+		public static void SetConditions(String[] conditions) {
+			Conditions = conditions;
+		}
+		
+		public static void GetConditions() {
+			for (String s : Conditions) {
+				System.out.print(s + " ");
+			}
+		}
+		
+		private static Boolean MatchConditions(File f)
+		{
+			if (Conditions == null)
+				return true;
+			for (int i = 0; i < Conditions.length; i++) {
+				String methodName = Conditions[i].substring(1);
+				String argumentName = "";
+				if ( i < Conditions.length -1 && Conditions[i + 1].charAt(0) != '-')
+				{
+					argumentName = Conditions[i + 1];
+					i++;
+				}
+				try {
+					Method m = MyFinder.class.getMethod("Match_" + methodName, File.class, String.class);
+					if (!((Boolean)m.invoke(MyFinder.class, f, argumentName)))
+					{
+						return false;
+					}
+				} catch (SecurityException e) {
+					e.printStackTrace();
+				} catch (NoSuchMethodException e) {
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+			return true;
+		}
+		
+		public static Boolean Match_readable(File f, String s)	{		return f.canRead();		}
+		public static Boolean Match_writable(File f, String s)	{		return f.canWrite();	}
+		public static Boolean Match_executable(File f, String s) {		return f.canExecute();	}		
+		public static Boolean Match_name(File f, String s) {
+			s = s.replace(".", "\\.");
+			s = s.replace("?", ".");
+			s = s.replace("*", ".*");
+			Matcher m = (Pattern.compile(s)).matcher(f.getName());
+			return m.matches();
+		}
+		public static Boolean Match_iname(File f, String s) {
+			s = s.replace(".", "\\.");
+			s = s.replace("?", ".");
+			s = s.replace("*", ".*");
+			Matcher m = (Pattern.compile(s)).matcher(f.getName());
+			return m.matches();
+		}
+		public static Boolean Match_regex(File f, String s) {
+			Matcher m = (Pattern.compile(s)).matcher(f.getName());
+			return m.matches();
+		}
+		public static Boolean Match_size(File f, String s){
+			if (s == "") return false;
+			int coeff = 1;
+			if (s.charAt(s.length() - 1) == 'k')
+			{
+				coeff = 1024;
+				s = s.substring(0, s.length() - 2);
+			}
+			else if (s.charAt(s.length() - 1) == 'M')
+			{
+				coeff = 1024 * 1024;
+				s = s.substring(0, s.length() - 2);
+			}
+			int size = Integer.parseInt(s) * coeff;
+			return f.getTotalSpace() == size;
+		}
+		public static Boolean Match_ssize(File f, String s){
+			if (s == "") return false;
+			int coeff = 1;
+			if (s.charAt(s.length() - 1) == 'k')
+			{
+				coeff = 1024;
+				s = s.substring(0, s.length() - 2);
+			}
+			else if (s.charAt(s.length() - 1) == 'M')
+			{
+				coeff = 1024 * 1024;
+				s = s.substring(0, s.length() - 2);
+			}
+			int size = Integer.parseInt(s) * coeff;
+			return f.getTotalSpace() <= size;
+		}
+		public static Boolean Match_bsize(File f, String s){
+			if (s == "") return false;
+			int coeff = 1;
+			if (s.charAt(s.length() - 1) == 'k')
+			{
+				coeff = 1024;
+				s = s.substring(0, s.length() - 2);
+			}
+			else if (s.charAt(s.length() - 1) == 'M')
+			{
+				coeff = 1024 * 1024;
+				s = s.substring(0, s.length() - 2);
+			}
+			int size = Integer.parseInt(s) * coeff;
+			return f.getTotalSpace() >= size;
+		}
+		public static Boolean Match_cnewer(File f, String s){
+			File ref = new File(s);
+			if (ref == null) 
+				return false;
+			else
+				return f.lastModified() > ref.lastModified();
+		}
+		public static Boolean Match_colder(File f, String s) {
+			File ref = new File(s);
+			if (ref == null) 
+				return false;
+			else
+				return f.lastModified() < ref.lastModified();
+		}
+		public static Boolean Match_cmin(File f, String s){
+			if (s=="") return false;
+			long Minutes = Integer.parseInt(s) * 60000;
+			long Current = Calendar.getInstance().getTimeInMillis();
+			return (Current - f.lastModified() > Minutes);
+		}
+		public static void RecursiveFind(String s)
+		{
+			
+			if (s.charAt(s.length() - 1) != '/')
+				s+= "/";
+			File dir = new File(s);
+			
+			if (MatchConditions(dir))
+			{
+				System.out.println(dir.getAbsolutePath());
+			}
+			
+			if (dir.isDirectory())
+			{
+				String[] fileNames = dir.list();
+				for (String name : fileNames) {
+					RecursiveFind(s + name);
+				}
+			}
+		}
+		
+		
+	}
+
+  public static void main(String[] argv) {
+    
+	  File dir = new File(argv[0]);	  
+	  String[] Conditions= new String[argv.length - 1];
+	  for (int i = 1; i < argv.length; i++) {
+		  Conditions[i - 1] = argv[i];
+	  }
+	  MyFinder.SetConditions(Conditions);
+	  MyFinder.RecursiveFind(argv[0]);
+
+	  //MyFinder.GetConditions();
+	  
+  }
+}
+
